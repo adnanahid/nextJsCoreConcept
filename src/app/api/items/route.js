fd113db50a96b810385fa258a3d01d50b1612247
adnanahid <sikuahamed@gmail.com>
2025-02-14 16:22:48 +0600

    done

---
+++ src/app/api/items/route.js
@@ -0,0 +1,19 @@
+export async function GET() {
+
+  const data = {
+    name: "Nahid Hasan",
+    age: 22,
+  };
+
+  return Response.json( data );
+}
+
+export async function POST() {
+
+  const data = {
+    name: "Nahid Hasan",
+    age: 22,
+  };
+
+  return Response.json( data );
+}
